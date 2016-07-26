bd7ab61a97ebf191341598d2b6c5dc8463f5ce60
Gitau 'Valdadyne' Rodgers <rodgers.gitau@gmail.com>
2016-07-26 17:26:21 +0300

    Update age.py

--- age.py
+++ age.py
@@ -1,15 +1,20 @@
 #Prompt for User Input
 #Use an elif statement
 
-Input= int(raw_input("What is Your Age?"))
-if Input > 0:                                #check if the age is a valid number!
-    if Input>30:
-        print("You're too old for this!")
-    elif Input>25:
-        print("You're getting too old!")
-    elif Input>18:
-        print("You're just the right age!")
-    else:
-        print("You're too young!")
+Name = str(raw_input("What is Your Name?"))
+Input = raw_input("What is Your Age?")
+if Name != "" and isinstance(Input,int ):
+    if Input > 0:                                #check for valid entries!
+        if Input > 80:
+            print(Name+" You're are an elder!")
+        elif Input>30:
+            print(Name+" You're too old for this!")
+        elif Input>25:
+            print(Name+" You're getting too old!")
+        elif Input>18:
+            print(Name+" You're just the right age!")
+        else:
+            print(Name+" You're too young!")
+
 else:
-    print("That's not a valid number")      #invalid age rejected!
+    print("Invalid Entry!")         #invalid entries rejected!
